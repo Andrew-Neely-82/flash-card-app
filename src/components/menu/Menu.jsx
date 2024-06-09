23fe13a7a1565158cc023f4503625d295541ba47
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-06-08 21:22:22 -0500

    menu toggle effects polished, card size more responsive

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,17 +1,33 @@
-import StyleIcon from "@mui/icons-material/Style";
 import LayersIcon from "@mui/icons-material/Layers";
 import SearchIcon from "@mui/icons-material/Search";
+import StyleIcon from "@mui/icons-material/Style";
+
+import CardSets from "./CardSets";
 import { useState } from "react";
 import Search from "./Search";
 import Icon from "./Icon";
-import CardSets from "./CardSets";
 
 const Menu = () => {
   // * Menu show handling
   const [open, setOpen] = useState(false);
+  const [menuTextDelay, setMenuTextDelay] = useState(false);
+
   const iconHandler = () => {
+    const listItem = document.querySelectorAll(".menuTextClass");
     setOpen(!open);
     setSearchOpen(false);
+
+    if (!open) {
+      listItem.forEach((item) => item.classList.remove("disappear"));
+      setTimeout(() => {
+        setMenuTextDelay(true);
+      }, 250);
+    } else {
+      listItem.forEach((item) => item.classList.add("disappear"));
+      setTimeout(() => {
+        setMenuTextDelay(false);
+      }, 250);
+    }
   };
 
   // * Search option show handling
@@ -26,21 +42,22 @@ const Menu = () => {
   const topicsHandler = () => {
     setOpen(false);
     setSearchOpen(false);
+    setMenuTextDelay(false);
     setTopicOpen(!topicOpen);
   };
 
   // * Class toggling
   const classes = {
     menuClass: open ? "open" : "closed",
-    menuTextClass: open ? "show" : "hidden",
-    searchClass: searchOpen ? "show" : "hidden",
-    topicsClass: topicOpen ? "show" : "hidden",
+    menuTextClass: menuTextDelay ? "show-menu-text" : "disappear d-none",
+    searchClass: searchOpen ? "show" : " d-none",
+    topicsClass: topicOpen ? "topics-show" : "topics-hidden",
   };
 
   return (
     <>
       <div className={`menu ${classes.menuClass}`}>
-        <button onClick={iconHandler} className="btn">
+        <button onClick={iconHandler} className="btn no-underline">
           <Icon iconHandler={iconHandler} />
         </button>
 
@@ -50,21 +67,21 @@ const Menu = () => {
           <li>
             <button className="btn" onClick={searchHandler}>
               <SearchIcon />
-              <span className={classes.menuTextClass}>Search</span>
+              <span className={`menuTextClass ${classes.menuTextClass}`}>Search</span>
             </button>
           </li>
 
           <li>
             <button className="btn" onClick={topicsHandler}>
               <LayersIcon />
-              <span className={classes.menuTextClass}>Cards Sets</span>
+              <span className={`menuTextClass ${classes.menuTextClass}`}>Cards Sets</span>
             </button>
           </li>
 
           <li>
             <button className="btn" onClick={iconHandler}>
               <StyleIcon />
-              <span className={classes.menuTextClass}>Tags</span>
+              <span className={`menuTextClass ${classes.menuTextClass}`}>Tags</span>
             </button>
           </li>
           <li></li>
